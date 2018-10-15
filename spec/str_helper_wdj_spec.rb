e5c9ce0817b4c61c92a3083ed85a0c5ccbadfe55
David Jungst <djungst@gmail.com>
2018-10-15 10:56:09 -0600

    added specs

--- spec/str_helper_wdj_spec.rb
+++ spec/str_helper_wdj_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe StrHelperWdj do
+  subject { StrHelperWdj }
+
   it "has a version number" do
     expect(StrHelperWdj::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
+    end
+
   end
+
 end
